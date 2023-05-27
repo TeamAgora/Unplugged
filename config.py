14aee91a1d53cc3004f3c713acb4ab42ff349a72
kimjikoin <130832705+kimjikoin@users.noreply.github.com>
2023-05-27 20:01:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Kimjikoin/xMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/teamagora")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/agoraworld")
