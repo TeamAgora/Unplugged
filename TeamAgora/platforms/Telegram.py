a662477abf3afff76f34429bc6b7080da6d4642d
kimjikoin <130832705+kimjikoin@users.noreply.github.com>
2023-05-27 19:54:05 +0530

    Update Telegram.py

--- TeamAgora/platforms/Telegram.py
+++ TeamAgora/platforms/Telegram.py
@@ -9,7 +9,7 @@ from pyrogram.types import (InlineKeyboardButton,
 
 import config
 from config import MUSIC_BOT_NAME, lyrical
-from AnonX import app
+from TeamAgora import app
 
 from ..utils.formatters import (convert_bytes, get_readable_time,
                                 seconds_to_min)
